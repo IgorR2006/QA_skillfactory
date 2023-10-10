79991ab13ffe4c68377679b70c4c3ae903384c16
IgorR2006 <146351982+IgorR2006@users.noreply.github.com>
2023-10-10 10:19:57 +0300

    Update HW17_7_3.py
    .

--- HW17_7_3.py
+++ HW17_7_3.py
@@ -2,10 +2,10 @@ d = []
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 money = float(input('Введите сумму:'))
-d.append(float(per_cent['ТКБ']*money/100))
-d.append(float(per_cent['СКБ']*money/100))
-d.append(float(per_cent['ВТБ']*money/100))
-d.append(float(per_cent['СБЕР']*money/100))
+d.append(round(float(per_cent['ТКБ']*money/100),2))
+d.append(round(float(per_cent['СКБ']*money/100),2))
+d.append(round(float(per_cent['ВТБ']*money/100),2))
+d.append(round(float(per_cent['СБЕР']*money/100),2))
 
 print(d)
 
